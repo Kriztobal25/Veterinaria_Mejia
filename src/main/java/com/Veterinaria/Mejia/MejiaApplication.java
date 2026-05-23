0f7ea446c41b72eabd46482e6ff8104488d183e4
Kriztobal25 <jackstauberteamo111@gmail.com>
2026-05-23 10:09:09 -0500

    chore: estructura base de spring boot con maven

---
+++ src/main/java/com/Veterinaria/Mejia/MejiaApplication.java
@@ -0,0 +1,13 @@
+package com.Veterinaria.Mejia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MejiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MejiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Veterinaria/Mejia/MejiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Veterinaria.Mejia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MejiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
